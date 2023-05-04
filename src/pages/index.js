03093a3adc53e755bc12e7a44d2012482644db30
Sergen Demir <dmrsrgn@gmail.com>
2023-05-04 09:19:56 -0500

    implement new function to get info from a child component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Response from '@/components/Response';
+import Form from '@/components/Form';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -8,46 +9,33 @@ export default function Home() {
   const [input, setInput] = useState('');
   const [response, setResponse] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const response = await fetch('/api/get-prompt-response', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ input }),
-    });
-    const { result } = await response.json();
-    setResponse(result);
+  const sendInput = (input) => {
+    setInput(input);
   };
+
+  const getCaption = async () => {
+    if (input) {
+      const response = await fetch('/api/get-prompt-response', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ input }),
+      });
+      const { result } = await response.json();
+      setResponse(result);
+    }
+  };
+
+  useEffect(() => {
+    getCaption();
+  }, [input]);
 
   return (
     <main
       className={`flex min-h-screen flex-col items-center p-24 gap-4 ${inter.className}`}
     >
-      <form
-        action="POST"
-        onSubmit={handleSubmit}
-        className="flex flex-col gap-2"
-      >
-        <label htmlFor="input">Input: </label>
-        <textarea
-          type="text"
-          id="input"
-          className="bg-gray-100 p-2"
-          value={input}
-          cols={30}
-          rows={5}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Enter a photo description..."
-        />
-        <input
-          type="submit"
-          value="Submit"
-          className="bg-red-100 rounded p-2"
-        />
-      </form>
+      <Form sendInput={sendInput} />
       <Response response={response} />
     </main>
   );
