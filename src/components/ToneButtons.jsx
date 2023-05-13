3d6844044ee80ee05d727f589c380467d96e0f41
Sergen Demir <dmrsrgn@gmail.com>
2023-05-13 09:22:01 -0500

    move state up

--- src/components/ToneButtons.jsx
+++ src/components/ToneButtons.jsx
@@ -2,22 +2,11 @@ import React, { useState } from 'react';
 import ToneButton from './ToneButton';
 
 const ToneButtons = () => {
-  const [tone, setTone] = useState('');
-  const [selectedTones, setSelectedTones] = useState({
-    professional: false,
-    casual: false,
-    friendly: false,
-  });
-
-  const handleToneChange = (tone) => {
-    alert(tone);
-  };
-
   return (
     <div className="flex gap-2 mx-auto justify-center">
-      <ToneButton tone="Professional" callback={handleToneChange} />
-      <ToneButton tone="Casual" callback={handleToneChange} />
-      <ToneButton tone="Friendly" callback={handleToneChange} />
+      <ToneButton tone="Casual" />
+      <ToneButton tone="Professional" />
+      <ToneButton tone="Friendly" />
     </div>
   );
 };
