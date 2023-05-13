dbddeb2a6b70b49befd8615ebb52b3e2cfcabec9
Sergen Demir <dmrsrgn@gmail.com>
2023-05-13 09:22:13 -0500

    add future direction

--- src/components/ToneButton.jsx
+++ src/components/ToneButton.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 
-const ToneButton = ({ tone, callback }) => {
+const ToneButton = ({ tone }) => {
   const [selected, setSelected] = useState(false);
   const style = 'p-1 bg-teal-100 text-black rounded';
   const activeStyle = 'p-1 bg-teal-600 text-white rounded';
 
   const handleClick = () => {
     setSelected((prev) => !prev);
-    callback(tone);
+    // get function from context to update state
   };
 
   return (
