d13066b6496239dd7fdb536caef47dc895d37c76
Sergen Demir <dmrsrgn@gmail.com>
2023-05-13 08:48:28 -0500

    add new state to track all tone selections

--- src/components/ToneButtons.jsx
+++ src/components/ToneButtons.jsx
@@ -3,13 +3,18 @@ import ToneButton from './ToneButton';
 
 const ToneButtons = () => {
   const [tone, setTone] = useState('');
+  const [selectedTones, setSelectedTones] = useState({
+    professional: false,
+    casual: false,
+    friendly: false,
+  });
 
   const handleToneChange = (tone) => {
     alert(tone);
   };
 
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-2 mx-auto justify-center">
       <ToneButton tone="Professional" callback={handleToneChange} />
       <ToneButton tone="Casual" callback={handleToneChange} />
       <ToneButton tone="Friendly" callback={handleToneChange} />
