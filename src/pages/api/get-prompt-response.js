28e9a6229777625ede8c15893fb2ff7b41ad4eec
Sergen Demir <dmrsrgn@gmail.com>
2023-05-15 22:52:48 -0500

    send tone data

--- src/pages/api/get-prompt-response.js
+++ src/pages/api/get-prompt-response.js
@@ -1,14 +1,14 @@
 import { openai } from '../../../openai';
 
 export default async function getPromptResponse(req, res) {
-  const { input } = req.body;
+  const { input, tone } = req.body;
 
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
     prompt: `You are helping people write better captions for their Instagram posts. 
     They will give you a description of the photo and information about the feel, vibe and the message they want to convey
     and you will return the caption. Limit the character count to max = 125.
-     Description of the photo:${input}`,
+    Description of the photo:${input}. The tone of the caption:${tone}`,
     temperature: 1,
     max_tokens: 250,
     top_p: 1,
