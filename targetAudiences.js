4c8772b493e505d28087428262fbb16b27cbcab6
Sergen Demir <dmrsrgn@gmail.com>
2023-05-17 23:12:57 -0500

    add data for target audiences

---
+++ targetAudiences.js
@@ -0,0 +1,84 @@
+const targetAudienceOptions = [
+  { name: 'Teens', category: 'Age' },
+  { name: 'Young adults', category: 'Age' },
+  { name: 'Adults', category: 'Age' },
+  { name: 'Middle-aged adults', category: 'Age' },
+  { name: 'Seniors', category: 'Age' },
+  { name: 'Male', category: 'Gender' },
+  { name: 'Female', category: 'Gender' },
+  { name: 'Non-binary', category: 'Gender' },
+  {
+    name: 'Fitness and wellness enthusiasts',
+    category: 'Interests/Hobbies',
+  },
+  {
+    name: 'Food and cooking enthusiasts',
+    category: 'Interests/Hobbies',
+  },
+  {
+    name: 'Travel and adventure seekers',
+    category: 'Interests/Hobbies',
+  },
+  {
+    name: 'Fashion and beauty enthusiasts',
+    category: 'Interests/Hobbies',
+  },
+  {
+    name: 'Technology and gaming enthusiasts',
+    category: 'Interests/Hobbies',
+  },
+  {
+    name: 'Music and entertainment lovers',
+    category: 'Interests/Hobbies',
+  },
+  {
+    name: 'Art and culture enthusiasts',
+    category: 'Interests/Hobbies',
+  },
+  {
+    name: 'Business and entrepreneurship',
+    category: 'Professional Fields',
+  },
+  { name: 'Education and academia', category: 'Professional Fields' },
+  {
+    name: 'Healthcare and wellness',
+    category: 'Professional Fields',
+  },
+  {
+    name: 'Creative arts and design',
+    category: 'Professional Fields',
+  },
+  { name: 'Science and technology', category: 'Professional Fields' },
+  { name: 'Sports and athletics', category: 'Professional Fields' },
+  { name: 'Students', category: 'Demographics' },
+  { name: 'Working professionals', category: 'Demographics' },
+  { name: 'Parents and caregivers', category: 'Demographics' },
+  { name: 'Retirees', category: 'Demographics' },
+  { name: 'Local community', category: 'Geographic Location' },
+  { name: 'National audience', category: 'Geographic Location' },
+  { name: 'International audience', category: 'Geographic Location' },
+  { name: 'English speakers', category: 'Language' },
+  { name: 'Non-English speakers', category: 'Language' },
+  { name: 'LGBTQ+ community', category: 'Niche Communities' },
+  { name: 'Environmental activists', category: 'Niche Communities' },
+  { name: 'Pet lovers', category: 'Niche Communities' },
+  { name: 'Bookworms', category: 'Niche Communities' },
+  { name: 'Gamers', category: 'Niche Communities' },
+  {
+    name: 'Health-conscious individuals',
+    category: 'Psychographic Traits',
+  },
+  {
+    name: 'Socially conscious individuals',
+    category: 'Psychographic Traits',
+  },
+  {
+    name: 'Tech-savvy individuals',
+    category: 'Psychographic Traits',
+  },
+  {
+    name: 'Creatively inclined individuals',
+    category: 'Psychographic Traits',
+  },
+  { name: 'All', category: 'All' },
+];
